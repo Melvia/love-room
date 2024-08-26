f5dcf8a8ae01add095f69460248e9c8f0a2f27c1
Nata <melvia.nr@gmail.com>
2024-08-26 22:32:33 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+let preMouseX: number | null = null;
+let preMouseY: number | null = null;
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,68 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'love-room';
+
+
+  // createHearts(x: number, y: number): void {
+  // createHearts(): void {
+  //   let currentEvent =  (event as MouseEvent);
+  //   let i = document.createElement('i');
+  //   document.body.appendChild(i);
+  //   i.style.top = currentEvent.pageY + 'px';
+  //   i.style.left = currentEvent.pageX + 'px';
+  //   setTimeout(()=> {
+  //     document.body.removeChild(i)
+  //   }, 2000)
+  // }
+
+  heart() {
+
+   const createHearts = (x: number, y: number ) => {
+     const getRandomValue = () => {
+       return `${Math.random() * 400 - 200}px`
+     }
+      let currentEvent =  (event as MouseEvent);
+      let i = document.createElement('i');
+      document.body.appendChild(i);
+      i.style.top = (currentEvent.pageY) + 'px';
+      i.style.left = (currentEvent.pageX) + 'px';
+      i.style.scale = `${Math.random() * 2 + 1}`;
+      i.style.setProperty('--x', getRandomValue());
+     i.style.setProperty('--y', getRandomValue());
+      setTimeout(()=> {
+      document.body.removeChild(i)
+    }, 2000);
+  }
+
+    let currentEvent =  (event as MouseEvent);
+    if (preMouseX !==null && preMouseY!==null) {
+
+      let deltaX = currentEvent.pageX - preMouseX;
+      let deltaY = currentEvent.pageY - preMouseY;
+      let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+       if (distance >= 50) {
+        createHearts(currentEvent.pageX, currentEvent.pageY);
+        preMouseX = currentEvent.pageX;
+        preMouseY = currentEvent.pageY;
+       }
+    }
+    else {
+        createHearts(currentEvent.pageX, currentEvent.pageY);
+        preMouseX = currentEvent.pageX;
+        preMouseY = currentEvent.pageY;
+      }
+    }
+
+
+
+
+
+  ngOnInit(): void {
+    this.heart.bind(this);
+    document.addEventListener('mousemove', this.heart);
+
+  }
+
 }
